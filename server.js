5895ac998d3f59a2493ba6fa55c9c4901c800350
Taneisha Stoll <taneishastoll@gmail.com>
2018-10-18 14:28:40 -0400

    Get the server running

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const server = require('socket.io')();
+const app = require('express')();
+const http = require('http').Server(app);
+const server = require('socket.io')(http);
 const firstTodos = require('./data');
 const Todo = require('./todo');
+
+// Serve the home page
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
 server.on('connection', (client) => {
     // This is going to be our fake 'database' for this application
@@ -10,7 +17,7 @@ server.on('connection', (client) => {
     // connections from the last time the server was run...
     const DB = firstTodos.map((t) => {
         // Form new Todo objects
-        return new Todo(title=t.title);
+        return new Todo(t.title);
     });
 
     // Sends a message to the client to reload all todos
@@ -36,4 +43,6 @@ server.on('connection', (client) => {
 });
 
 console.log('Waiting for clients to connect');
-server.listen(3003);
+http.listen(3003, () => {
+  console.log('Serving on localhost:3003');
+});
